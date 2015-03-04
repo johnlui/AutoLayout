3dd12e7e0ce3919580aed690e45304521bff33d0
JohnLui <wenhanlv@gmail.com>
2015-03-04 19:57:02 +0800

    Auto Layout 使用心得（一）【Swift on iOS 学习笔记】

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by JohnLui on 15/2/24.
+//  Copyright (c) 2015年 Miao Tec Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
