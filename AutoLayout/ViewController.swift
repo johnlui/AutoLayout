09af5f7b7548b0edb6c3ffc9bfc4bd4485d930d4
JohnLui <wenhanlv@gmail.com>
2015-04-01 18:26:49 +0800

    Auto Layout 使用心得（四）—— 22 行代码实现拖动回弹

--- AutoLayout/ViewController.swift
+++ AutoLayout/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var middleViewTopSpaceLayoutConstant: CGFloat!
+    var middleViewOriginY: CGFloat!
+    
+    @IBOutlet weak var middleView: UIView!
+    @IBOutlet weak var middleViewTopSpaceLayout: NSLayoutConstraint!
+    @IBOutlet var panGesture: UIPanGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        panGesture.addTarget(self, action: Selector("pan"))
+        middleViewTopSpaceLayoutConstant = middleViewTopSpaceLayout.constant
+        middleViewOriginY = middleView.frame.origin.y
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func pan() {
+        if panGesture.state == UIGestureRecognizerState.Ended {
+            UIView.animateWithDuration(0.4, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                self.middleView.frame.origin.y = self.middleViewOriginY
+                }, completion: { (success) -> Void in
+                    if success {
+                        self.middleViewTopSpaceLayout.constant = self.middleViewTopSpaceLayoutConstant
+                    }
+            })
+            return
+        }
+        let y = panGesture.translationInView(self.view).y
+        middleViewTopSpaceLayout.constant = middleViewTopSpaceLayoutConstant + y
+    }
 
 }
 
